b9844ba4233202a93f3a4cd1732e8fa690c49ff4
paranerd <paranerd.development@gmail.com>
2023-08-28 14:51:25 +0200

    Debugging postcreate

--- service/postcreate.sh
+++ service/postcreate.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "postcreate.sh has been called"
 
-gcloud run services update $SERVICE_NAME --update-env-vars SERVICE_URL=$SERVICE_URL
+gcloud run services update $SERVICE_NAME --update-env-vars SERVICE_URL_TWO=$SERVICE_URL

96ead6428714c2ab766ac213a9b8264794691b34
paranerd <paranerd.development@gmail.com>
2023-08-28 16:00:04 +0200

    Fixed command to get Service info

--- service/postcreate.sh
+++ service/postcreate.sh
@@ -4,8 +4,7 @@ echo "postcreate.sh has been called"
 
 echo "Service Name: " $SERVICE_NAME
 echo "Service URL: " $SERVICE_URL
-URL=$(gcloud run services describe SERVICE --platform managed --region REGION --format 'value(status.url)'
-)
+URL=$(gcloud run services describe cloud-run-test --platform managed --format 'value(status.url)' --project $GOOGLE_CLOUD_PROJECT --region $GOOGLE_CLOUD_REGION)
 echo "URL: " $URL
 
 gcloud run services update $SERVICE_NAME --update-env-vars SERVICE_URL_TWO=$SERVICE_URL --region $GOOGLE_CLOUD_REGION

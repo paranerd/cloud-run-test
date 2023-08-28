74c4a139aab609616295c1358e111a09662c461a
paranerd <paranerd.development@gmail.com>
2023-08-28 15:45:54 +0200

    Added region to gcloud command

--- service/postcreate.sh
+++ service/postcreate.sh
@@ -2,4 +2,4 @@
 
 echo "postcreate.sh has been called"
 
-gcloud run services update $SERVICE_NAME --update-env-vars SERVICE_URL_TWO=$SERVICE_URL
+gcloud run services update $SERVICE_NAME --update-env-vars SERVICE_URL_TWO=$SERVICE_URL --region $GOOGLE_CLOUD_REGION

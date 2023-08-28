963dbcb7019261809ce5d2b569572e3708412558
paranerd <paranerd.development@gmail.com>
2023-08-28 15:56:51 +0200

    postcreate debugging

--- service/postcreate.sh
+++ service/postcreate.sh
@@ -2,4 +2,10 @@
 
 echo "postcreate.sh has been called"
 
+echo "Service Name: " $SERVICE_NAME
+echo "Service URL: " $SERVICE_URL
+URL=$(gcloud run services describe SERVICE --platform managed --region REGION --format 'value(status.url)'
+)
+echo "URL: " $URL
+
 gcloud run services update $SERVICE_NAME --update-env-vars SERVICE_URL_TWO=$SERVICE_URL --region $GOOGLE_CLOUD_REGION

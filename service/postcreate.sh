44e6fc1c6de011c71cc2e168f32bd54522a7200f
paranerd <paranerd.development@gmail.com>
2023-08-29 10:55:08 +0200

    Added prebuild script

--- service/postcreate.sh
+++ service/postcreate.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Install Node dependencies
-npm i --prefix ../service
-
-# Transpile JS to TS
-npm exec --prefix ../service tsc
-
 # Run Terraform deployment
 terraform -chdir=../terraform/ init
 terraform -chdir=../terraform/ apply -auto-approve
---
+++ service/prebuild.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install Node dependencies
+npm i
+
+# Transpile JS to TS
+npm exec tsc
